9410ca2dc2fc4811784d8dc7ac816e163f84a7cc
gadhwal <gadhwal@yahoo.com>
2016-01-30 18:32:59 +0530

    Assignment 1 or 2

---
+++ Assignment2/IfAge.java
@@ -0,0 +1,19 @@
+public class IfAge
+{
+	public static void main (String [] args)
+	{
+	
+	int a=Integer.parseInt(args[0]);
+	if (a>=18)
+		{
+		System.out.println(a+" Is eligible for Election");
+		}
+		else
+		{
+		System.out.println(a+"Is Not eligible for Election");	
+		}
+	}
+	
+}
+	
+	
