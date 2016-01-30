6d408e54dbb0d2d1d4b25e00563030bcfdbcdbf2
gadhwal <gadhwal@yahoo.com>
2016-01-30 13:40:29 +0530

    Assignment 2 or 3

---
+++ VariableDeclaration.java
@@ -0,0 +1,21 @@
+public class VariableDeclaration
+{
+		
+		public static void main(String Args[])
+		{
+		int a = 10;
+		String s="Hello This is Sets of characters";
+		long l = 550000;
+		double d = 25 * 1.5;
+	
+		{
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(d);
+		}
+		
+		}
+}
+
+
