5977a456d749f96a3f254e6aed8661b68416bff4
gadhwal <gadhwal@yahoo.com>
2016-01-31 19:12:17 +0530

    Session 2 Assignment 4

---
+++ Assignment2/SwitchAssignment.java
@@ -0,0 +1,51 @@
+public class SwitchAssignment
+
+{
+	public static void main (String [] args)
+	
+	{
+		//char M = '1';
+		int m=Integer.parseInt(args[0]);;
+		switch (m)
+		{
+			case 1:
+			System.out.println("31");
+			break;
+			case 2:
+			System.out.println("28");
+			break;
+			case 3:
+			System.out.println("31");
+			break;
+			case 4:
+			System.out.println("30");
+			break;
+			case 5:
+			System.out.println(31);
+			break;
+			
+			case 6: System.out.println(30);break;
+			case 7: System.out.println(31);break;
+			case 8: System.out.println(31);break;
+			case 9: System.out.println(30);break;
+			case 10: System.out.println(31);break;
+			case 11: System.out.println(30);break;
+			case 12: System.out.println(31);break;
+		
+
+
+		default:
+		System.out.println("Invalid Month");
+		
+			
+		}
+	
+		
+		
+		
+	}
+	
+	
+	
+}
+
